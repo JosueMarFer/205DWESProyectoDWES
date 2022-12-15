cb8f0ef48a3b551cb314bb5abcb932730062cc87
ZenOne <ZenOne@DESKTOP-V49KOCS>
2022-12-15 05:28:12 +0100

    15/12/2022
    1.0

---
+++ indexProyectoDWES.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="./webroot/css/style.css">
+        <title>205ProyectoDWES</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>&lt;/DWES&gt;</h1>
+            <h2>Desarrollo Web en Entorno Servidor</h2>
+        </header>
+        <main>
+            <section>   
+                <a href="./doc/EstudioTema1.pdf" target="_blank">
+                    <h3>TEMA 1: Desarrollo web en entorno servidor</h3>
+                </a>
+                <a href="./doc/EstudioTema2.pdf" target="_blank">
+                    <h3>
+                        TEMA 2: Instalación, configuración y documentación del entorno de
+                        desarrollo y del entorno de explotación
+                    </h3>
+                </a>
+                <a href="../205DWESProyectoTema3/indexProyectoTema3.php">
+                    <h3>TEMA 3: Características del lenguaje php</h3>
+                </a>
+                <a href="../205DWESProyectoTema4/indexProyectoTema4.php">
+                    <h3>TEMA 4: Técnicas de acceso a datos en php</h3>
+                </a>
+                <a href="../205DWESProyectoTema5/indexProyectoTema5.php">
+                    <h3>
+                        TEMA 5: Desarrollo de aplicaciones web utilizando código embebido
+                    </h3>
+                </a>
+                <a href="../205DWESProyectoLoginLogoffTema5/codigoPHP/login.php">
+                    <h3>
+                        TEMA 5: Proyecto LoginLogoff
+                    </h3>
+                </a>
+                <a href="#">
+                    <h3>TEMA 6: Programación orientada a objetos en php</h3>
+                </a>
+                <a href="#">
+                    <h3>TEMA 7: Programación de servicios web</h3>
+                </a>
+                <a href="#">
+                    <h3>TEMA 8: Desarrollo de aplicaciones web híbridas</h3>
+                </a>
+                <a href="#">
+                    <h3>TEMA 9: Desarrollo de aplicaciones web híbridas</h3>
+                </a>
+            </section>
+            <div class="return">
+                <a href="../index.html"><img src="./webroot/images/back.png" alt="Imagen back"></a>
+            </div>
+        </main>
+        <footer>
+            <div class="footerIcons">
+                <a href="./doc/curriculum.pdf" target="_blank"><img src="./webroot/images/curriculum.png" alt="Imagen curriculum"></a>
+                <a href="https://github.com/JosueMarFer/205DWESProyectoDWES" target="_blank"><img src="./webroot/images/github.png"
+                                                                                                  alt="Imagen github"></a>
+            </div>
+            <div class="home">
+                <a href="../index.html"><img src="./webroot/images/home.png" alt="Imagen home"></a>
+                <p>Josué martínez Fernández</p>
+            </div>
+        </footer>
+    </body>
+
+</html>
